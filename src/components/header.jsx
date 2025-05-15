2e4ad5e808917ef646961fafc5b9e04126d9d3f7
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2025-05-15 09:19:09 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,56 +13,58 @@ const Header = () => {
   const { user } = useUser();
 
   return (
-    <>
-      <nav className="py-4 flex justify-between items-center">
-        <Link to="/">
-          <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
-        </Link>
+    <nav className="py-4 flex justify-between items-center">
+      <Link to="/">
+        <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+      </Link>
 
-        <div className="flex gap-8">
-          <SignedOut>
-            <SignInButton mode="modal" redirectUrl="/onboarding">
-              <Button variant="outline">Login</Button>
-            </SignInButton>
-          </SignedOut>
+      <div className="flex gap-8">
+        <SignedOut>
+          <SignInButton
+            mode="modal"
+            redirectUrl="/onboarding"
+            signUpForceRedirectUrl="/onboarding"
+          >
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
 
-          <SignedIn>
-            {user?.unsafeMetadata?.role === "recruiter" && (
-              <Link to="/post-job">
-                <Button variant="destructive" className="rounded-full">
-                  <PenBox size={20} className="mr-2" />
-                  Post a Job
-                </Button>
-              </Link>
-            )}
-
-            <UserButton
-              appearance={{
-                elements: {
-                  avatarBox: "w-10 h-10",
-                },
-              }}
-              userProfileMode="modal"
-              afterSignOutUrl="/"
-            >
-              <UserButton.MenuItems>
-                <UserButton.Link
-                  label="My Jobs"
-                  labelIcon={<BriefcaseBusiness size={15} />}
-                  href="/my-jobs"
-                />
-                <UserButton.Link
-                  label="Saved Jobs"
-                  labelIcon={<Heart size={15} />}
-                  href="/saved-jobs"
-                />
-              </UserButton.MenuItems>
-            </UserButton>
-          </SignedIn>
-        </div>
-      </nav>
-    </>
+        <SignedIn>
+          {user?.unsafeMetadata?.role === "recruiter" && (
+            <Link to="/post-job">
+              <Button variant="destructive" className="rounded-full">
+                <PenBox size={20} className="mr-2" />
+                Post a Job
+              </Button>
+            </Link>
+          )}
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "w-10 h-10",
+              },
+            }}
+            userProfileMode="modal"
+            afterSignOutUrl="/"
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="My Jobs"
+                labelIcon={<BriefcaseBusiness size={15} />}
+                href="/my-jobs"
+              />
+              <UserButton.Link
+                label="Saved Jobs"
+                labelIcon={<Heart size={15} />}
+                href="/saved-jobs"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </SignedIn>
+      </div>
+    </nav>
   );
 };
 
 export default Header;
+
