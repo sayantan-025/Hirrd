40711a02e5c45fe51dcff972ceed422be5443434
Sayantan Bera <138392672+sayantan-025@users.noreply.github.com>
2025-05-15 09:24:21 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,70 +1,90 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import {
   SignedIn,
   SignedOut,
   UserButton,
-  SignInButton,
+  SignIn,
   useUser,
 } from "@clerk/clerk-react";
 import { Button } from "./ui/button";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const [search, setSearch] = useSearchParams();
   const { user } = useUser();
 
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
+  };
+
   return (
-    <nav className="py-4 flex justify-between items-center">
-      <Link to="/">
-        <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
-      </Link>
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+        </Link>
 
-      <div className="flex gap-8">
-        <SignedOut>
-          <SignInButton
-            mode="modal"
-            redirectUrl="/onboarding"
+        <div className="flex gap-8">
+          <SignedOut>
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            {user?.unsafeMetadata?.role === "recruiter" && (
+              <Link to="/post-job">
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            >
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="My Jobs"
+                  labelIcon={<BriefcaseBusiness size={15} />}
+                  href="/my-jobs"
+                />
+                <UserButton.Link
+                  label="Saved Jobs"
+                  labelIcon={<Heart size={15} />}
+                  href="/saved-jobs"
+                />
+                <UserButton.Action label="manageAccount" />
+              </UserButton.MenuItems>
+            </UserButton>
+          </SignedIn>
+        </div>
+      </nav>
+
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
             signUpForceRedirectUrl="/onboarding"
-          >
-            <Button variant="outline">Login</Button>
-          </SignInButton>
-        </SignedOut>
-
-        <SignedIn>
-          {user?.unsafeMetadata?.role === "recruiter" && (
-            <Link to="/post-job">
-              <Button variant="destructive" className="rounded-full">
-                <PenBox size={20} className="mr-2" />
-                Post a Job
-              </Button>
-            </Link>
-          )}
-          <UserButton
-            appearance={{
-              elements: {
-                avatarBox: "w-10 h-10",
-              },
-            }}
-            userProfileMode="modal"
-            afterSignOutUrl="/"
-          >
-            <UserButton.MenuItems>
-              <UserButton.Link
-                label="My Jobs"
-                labelIcon={<BriefcaseBusiness size={15} />}
-                href="/my-jobs"
-              />
-              <UserButton.Link
-                label="Saved Jobs"
-                labelIcon={<Heart size={15} />}
-                href="/saved-jobs"
-              />
-            </UserButton.MenuItems>
-          </UserButton>
-        </SignedIn>
-      </div>
-    </nav>
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
 export default Header;
+
 
